f99f9aa7d5c8097308646f9a41001bf3c31879ea
GustavoAlvesDeOliveira <gustavo.aoliveira@sptech.school>
2025-08-25 12:18:47 -0300

    Correção da imagem do login e personalização do app.js

--- full-website/app.js
+++ full-website/app.js
@@ -25,23 +25,30 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
 
 
-
 app.use("/usuarios", usuarioRouter);
 
 
 app.listen(PORTA_APP, function () {
     console.log(`
-    ##   ##  ######   #####             ####       ##     ######     ##              ##  ##    ####    ######  
-    ##   ##  ##       ##  ##            ## ##     ####      ##      ####             ##  ##     ##         ##  
-    ##   ##  ##       ##  ##            ##  ##   ##  ##     ##     ##  ##            ##  ##     ##        ##   
-    ## # ##  ####     #####    ######   ##  ##   ######     ##     ######   ######   ##  ##     ##       ##    
-    #######  ##       ##  ##            ##  ##   ##  ##     ##     ##  ##            ##  ##     ##      ##     
-    ### ###  ##       ##  ##            ## ##    ##  ##     ##     ##  ##             ####      ##     ##      
-    ##   ##  ######   #####             ####     ##  ##     ##     ##  ##              ##      ####    ######  
-    \n\n\n                                                                                                 
-    Servidor do seu site já está rodando! Acesse o caminho a seguir para visualizar .: http://${HOST_APP}:${PORTA_APP} :. \n\n
-    Você está rodando sua aplicação em ambiente de .:${process.env.AMBIENTE_PROCESSO}:. \n\n
-    \tSe .:desenvolvimento:. você está se conectando ao banco local. \n
-    \tSe .:producao:. você está se conectando ao banco remoto. \n\n
-    \t\tPara alterar o ambiente, comente ou descomente as linhas 1 ou 2 no arquivo 'app.js'\n\n`);
+    /^^^^              /^^^ /^^^^^^                        /^^     
+  /^^    /^^                /^^                            /^^     
+/^^        /^^/^^ /^^       /^^    /^ /^^^   /^^       /^^^/^^  /^^
+/^^        /^^ /^^  /^^     /^^     /^^    /^^  /^^  /^^   /^^ /^^ 
+/^^        /^^ /^^  /^^     /^^     /^^   /^^   /^^ /^^    /^/^^   
+  /^^     /^^  /^^  /^^     /^^     /^^   /^^   /^^  /^^   /^^ /^^ 
+    /^^^^     /^^^  /^^     /^^    /^^^     /^^ /^^^   /^^^/^^  /^^
+                                                                   
+  /^^ ^^                    /^^                                    
+/^^    /^^                  /^^                                    
+ /^^      /^^   /^^ /^^^^ /^/^ /^   /^^    /^^^ /^^ /^^  /^^^^     
+   /^^     /^^ /^^ /^^      /^^   /^   /^^  /^^  /^  /^^/^^        
+      /^^    /^^^    /^^^   /^^  /^^^^^ /^^ /^^  /^  /^^  /^^^     
+/^^    /^^    /^^      /^^  /^^  /^         /^^  /^  /^^    /^^    
+  /^^ ^^     /^^   /^^ /^^   /^^   /^^^^   /^^^  /^  /^^/^^ /^^    Based: WEB-DATA-VIZ
+           /^^                                                     
+    \n\n
+                                                                       
+    OnTrack Systems Rodando: http://${HOST_APP}:${PORTA_APP} :. \n\n
+    Você está rodando sua aplicação em ambiente de .:${process.env.AMBIENTE_PROCESSO}:. \n
+    `);
 });
