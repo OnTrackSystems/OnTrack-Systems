9b3a075a38bef6b259b4df97410fc3dfadbeb12c
GustavoAlvesDeOliveira <gustavo.aoliveira@sptech.school>
2025-09-01 01:03:01 -0300

    Rotas de cadastro funcionando

--- full-website/src/controllers/usuarioController.js
+++ full-website/src/controllers/usuarioController.js
@@ -48,21 +48,26 @@ function cadastrar(req, res) {
     var nome = req.body.nomeServer;
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
-    var fkEmpresa = req.body.idEmpresaVincularServer;
+    var cpf = req.body.cpfServer;
+    var telefone = req.body.telefoneServer
+    var cargo = req.body.cargoServer;
+    var funcao = req.body.funcaoServer;
+    var fkEmpresa = req.body.empresaServer;
 
-    // Faça as validações dos valores
-    if (nome == undefined) {
-        res.status(400).send("Seu nome está undefined!");
-    } else if (email == undefined) {
-        res.status(400).send("Seu email está undefined!");
-    } else if (senha == undefined) {
-        res.status(400).send("Sua senha está undefined!");
-    } else if (fkEmpresa == undefined) {
-        res.status(400).send("Sua empresa a vincular está undefined!");
-    } else {
+
+    // // Faça as validações dos valores
+    // if (nome == undefined) {
+    //     res.status(400).send("Seu nome está undefined!");
+    // } else if (email == undefined) {
+    //     res.status(400).send("Seu email está undefined!");
+    // } else if (senha == undefined) {
+    //     res.status(400).send("Sua senha está undefined!");
+    // } else if (fkEmpresa == undefined) {
+    //     res.status(400).send("Sua empresa a vincular está undefined!");
+    // } else {
 
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha, fkEmpresa)
+        usuarioModel.cadastrar(nome, email, senha, cpf, telefone, cargo, funcao, fkEmpresa)
             .then(
                 function (resultado) {
                     res.json(resultado);
@@ -77,8 +82,8 @@ function cadastrar(req, res) {
                     res.status(500).json(erro.sqlMessage);
                 }
             );
-    }
 }
+
 function buscarUsuarios(req, res){
     var idEmpresa = req.params.idEmpresa;
 
--- full-website/src/models/usuarioModel.js
+++ full-website/src/models/usuarioModel.js
@@ -10,13 +10,14 @@ function autenticar(email, senha) {
 }
 
 // Coloque os mesmos parâmetros aqui. Vá para a var instrucaoSql
-function cadastrar(nome, email, senha, fkEmpresa) {
-    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha, fkEmpresa);
+function cadastrar(nome, email, senha, cpf, telefone, cargo,funcao, fkEmpresa) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha, cpf, telefone, cargo,funcao, fkEmpresa);
     
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
-    var instrucaoSql = `
-        INSERT INTO usuario (nome, email, senha, fkempresa) VALUES ('${nome}', '${email}', '${senha}', '${fkEmpresa}');
+    var instrucaoSql = `insert into usuarios (nome,email,senha,cpf,telefone,cargo,funcao,fkEmpresa)
+                        values ("${nome}","${email}","${senha}",${cpf},${telefone},"${cargo}","${funcao}",${fkEmpresa});
+        
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
