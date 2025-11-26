75fceda59f313377b59c32ecac7448617c5536ee
ThiagoFukunaga <thiago.fukunaga@sptech.school>
2025-11-26 18:46:42 -0300

    feat: puxando os chamados do S3

--- src/controllers/chamadosController.js
+++ src/controllers/chamadosController.js
@@ -5,6 +5,37 @@ function listarChamados(req,res){
         res.status(200).json(resultado);
     });
 }
+
+const { S3Client, GetObjectCommand } = require('@aws-sdk/client-s3');
+
+async function getCallsFromBucket(req, res){
+    const s3Client = new S3Client({
+        region: "us-east-1",
+        credentials: {
+            // é string mas qnd for subir p ec2 da pra colocar var de ambiente!!
+            accessKeyId:"",
+            secretAccessKey:"",
+            sessionToken:""
+        }
+    });
+
+    const input = {
+        Bucket: "awsontrackclient",
+        Key: "chamados_dashboard.json"
+    }
+
+    const command = new GetObjectCommand(input);
+    const response = await s3Client.send(command);
+    const bytes = await response.Body.transformToByteArray();
+
+    const jsonString = Buffer.from(bytes).toString("utf-8");
+    const data = JSON.parse(jsonString);
+
+    console.log(data)
+    return res.status(200).json(data)
+}
+
 module.exports={
+    getCallsFromBucket,
     listarChamados
 }
--- src/routes/chamados.js
+++ src/routes/chamados.js
@@ -6,4 +6,9 @@ let chamadosController = require('../controllers/chamadosController');
 router.get("/listarChamados/", function(req,res){
     chamadosController.listarChamados(req,res)
 })
+
+router.get("/getCallsFromBucket/", function(req,res){
+    chamadosController.getCallsFromBucket(req,res)
+})
+
 module.exports = router;
