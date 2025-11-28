8d7c8dbcc7c6c4c7893c178a1669188c91c9bc51
ThiagoFukunaga <thiago.fukunaga@sptech.school>
2025-11-28 10:28:05 -0300

    feat:Severidade atual sendo atualizada do S3 - Dashboard Chamados

--- src/controllers/chamadosController.js
+++ src/controllers/chamadosController.js
@@ -13,9 +13,9 @@ async function getCallsFromBucket(req, res){
     const s3Client = new S3Client({
         region: "us-east-1",
          credentials: {
-            accessKeyId: "",
-            secretAccessKey: "",
-            sessionToken: ""
+            accessKeyId: "ASIA4WILKHQM7ZMLQ5BD",
+            secretAccessKey: "XsKG29wvD9pT1yMHFQSv1NSWzHPKikUpayhvFOhq",
+            sessionToken: "IQoJb3JpZ2luX2VjEO3//////////wEaCXVzLXdlc3QtMiJHMEUCIC8RoOGXvtgQNMpXYV7OnmtX774VfJULXSxzjE3YaTBXAiEAtnqaBdIrqTLPEGbcXa6zPRZ5h9g/DRRAbYjZe13JyU0qwAIItv//////////ARABGgw4NzI0Mzg5NzE0MTciDLMhBSHHEqYaRwSZeiqUAk8r+U/1W2t+PLmdyJgwvIssy6DE462ThrGz5caJFm6EE4IvomsoRVaO2M0hKbbWa+nvUitu+w+cI6kgovYbf6bSojzWyGYnBTziJWIhfmz4UVRKooNbyN+hNEuFLShvt2TtN/XeHzEw2V09DeBLQFM5FmB4j4IYY0oQd3vUEC8hKLqlUm28LwtFpUNIvGRlD8YxYU5rbgebzyKrDm7EfCvHq520LCPSgnu6lbscm8Ryb7oe8vZ2HaWPMxbi3ba7SVM50d3rbGZG44dAznmR5WQGg7TFzePOQqqvGUAS3/DbFBDDzrQCDALFDND6Zto/+eeW0d5USfVNSI6mu04Sw6oG8/gQ8CbRvmPvGn6VUy0dyLDMOTD4vqbJBjqdAe5Ej2DSU/lBZgBvBt0ekpGNk6l/JnH5AcosZVkF1JO3lwhp+ZvtVNNthhrKtYRLsYRTd1hDkW9lFx4IDZO1OaR0t4XTPfrq2093k3OCzpbUfGWGCT0mV8r2KhclhqUdWIMUTn40HGM1eJS7gwAxQhBM03oKuxJGtwxsGjVnGMjLIZfP6ImzjsmeubzgQrfxpSrze/t2hHacWdQ4qDw="
         } // tem que configuar isso aq pra puxar do bucket
     });
 
